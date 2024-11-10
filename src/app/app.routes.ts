f7db067422f418fb98b0738eafa434deea789c32
Ikemo4 <67703806+Ikemo4@users.noreply.github.com>
2024-11-09 23:00:04 -0700

    GOT THE NAVIGATION WORKING

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { ShadoweeListComponent } from './shadowee-list/shadowee-list.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '/intro', component: IntroLandingComponent},
-    { path: '/shadowees', component: ShadoweeListComponent },
-    { path: '/', redirectTo: '/intro', pathMatch: 'full'},
+    { path: 'intro', component: IntroLandingComponent},
+    { path: 'shadowees', component: ShadoweeListComponent },
+    { path: '', redirectTo: 'intro', pathMatch: 'full'},
     // to-do { path: '**', component: PageNotFoundComponent }
 ];
